1ae5de83735608a5f8f7178a66dcd2f0985b8802
Junior Oliveira <franciscoanto@gmail.com>
2021-11-04 09:50:05 -0300

    removendo cosolelog e comentando .js

--- saude/login/resources/js/saude.js
+++ saude/login/resources/js/saude.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+    //url atual de referencia
     var url = window.location.href;
-    console.log({url})
+    //string que deve ser encontrada
     var stringGrant = 'required-action';
-    console.log(url.includes(stringGrant));
+    //verificando se existe a string e retornando TRUE ou FALSE
     var verify = url.includes(stringGrant);
-    $("#loginOrRegisterSaude").show();
+    //mostrando o conteúdo
+    $(".loginOrRegisterSaude").show();
+    //ocultando consentimento
     $("#grantSaude").hide();
+    //condicional para mostrar e ocutar conteudo
     if(verify) {
         $(".loginOrRegisterSaude").hide();
         $("#grantSaude").show();
